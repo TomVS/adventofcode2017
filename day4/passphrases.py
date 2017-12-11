4bd2785cf930bd790735e1840e7df3521e5524f2
Thomas Vincent-Sweet <thomas.vincent-sweet@motionpicturesolutions.com>
2017-12-11 16:22:58 +0000

    day 4 part 2 solution

--- day4/passphrases.py
+++ day4/passphrases.py
@@ -4,6 +4,7 @@ import sys
 
 def passphraseValidCount(data):
     totalValid = 0
+    totalValid2 = 0
     for line in data:
 
         split = line.split()
@@ -12,7 +13,24 @@ def passphraseValidCount(data):
 
         if origLength == setLength:
             totalValid += 1
-    return totalValid
+# now check for anagram cases (totalValid2)
+            itemsMap = {}
+            itemsSet = {}
+            for token in split:
+                l = len(token)
+                if l not in itemsMap.keys():
+                    itemsMap[l] = []
+                    itemsSet[l] = set()
+                itemsMap[l].append(token)
+                orderedToken = "".join(sorted(token))
+                itemsSet[l].add(orderedToken)
+            allValid = True
+            for k,v in itemsMap.items():
+                if len(v) != len(itemsSet[k]):
+                    allValid = False
+            if allValid:
+                totalValid2 += 1
+    return totalValid, totalValid2
 
 def main():
     if len(sys.argv) < 2:
@@ -22,7 +40,7 @@ def main():
     with open(sys.argv[1]) as f:
         data = f.readlines()
 
-    print "Total Valid:", passphraseValidCount(data)
+    print "Total Valid (part1, part2):", passphraseValidCount(data)
 
 
 if __name__ == '__main__':
